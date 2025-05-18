d0182c75392974f76074612f7e8686e6404ad86b
QASIM07J <mqasimyaz07@gmail.com>
2025-05-18 18:40:59 +0500

    Final Version IA

--- ubuntu@ec2-13-201-134-227.ap-south-1.compute.amazonaws.com/task_manager/application.py
+++
@@ -1,6 +0,0 @@
-from app import app, init_db
-application = app  # EB uses 'application' as the WSGI entry point
-
-if __name__ == '__main__':
-    init_db()
-    application.run(host='0.0.0.0', port=5000, debug=True)
--- ubuntu@ec2-13-201-134-227.ap-south-1.compute.amazonaws.com/task_manager/static/js/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const taskItems = document.querySelectorAll('.task-item');
-    taskItems.forEach(item => {
-        item.addEventListener('click', () => {
-            item.classList.add('bounce');
-            setTimeout(() => item.classList.remove('bounce'), 300);
-        });
-    });
-});
